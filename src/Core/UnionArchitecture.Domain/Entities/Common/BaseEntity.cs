2d31881781805d29f349962321186e024f3c5c4e
Kerimov056 <ulvisk@code.edu.az>
2023-07-18 12:12:00 -0700

    first commit

---
+++ src/Core/UnionArchitecture.Domain/Entities/Common/BaseEntity.cs
@@ -0,0 +1,9 @@
+﻿namespace UnionArchitecture.Domain.Entities.Common;
+
+public abstract class BaseEntity
+{
+    public Guid Id { get; set; }
+    public DateTime CreatedDate { get; set; }
+    public DateTime ModifiedDate { get; set;}
+    public virtual bool IsDeleted { get; set;}  
+}
