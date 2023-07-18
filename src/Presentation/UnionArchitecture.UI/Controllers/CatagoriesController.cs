8603597c0fe451a1624cad3c8e8d4d96b0592b1c
Kerimov056 <ulvisk@code.edu.az>
2023-07-18 12:31:15 -0700

    Controller>>Catagories

---
+++ src/Presentation/UnionArchitecture.UI/Controllers/CatagoriesController.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace UnionArchitecture.UI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class CatagoriesController : ControllerBase
+    {
+        [HttpGet]
+        public string Get()
+        {
+            return "Salam";
+        }
+    }
+}
