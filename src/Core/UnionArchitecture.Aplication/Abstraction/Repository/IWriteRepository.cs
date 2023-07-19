1ab02c531ed09a8ae5e74ddcf2742a56f1d771ee
Kerimov056 <ulvisk@code.edu.az>
2023-07-19 08:41:12 -0700

    BE-007 IRepository and IReadRepository and IWriteRepository

---
+++ src/Core/UnionArchitecture.Aplication/Abstraction/Repository/IWriteRepository.cs
@@ -0,0 +1,13 @@
+﻿using UnionArchitecture.Domain.Entities.Common;
+
+namespace UnionArchitecture.Aplication.Abstraction.Repository;
+
+public interface IWriteRepository<T>:IRepository<T> where T : BaseEntity,new()
+{
+    Task AddAsync(T entity); // bir entity gelir servicesden son onu add edirsen bu qeder Sade 
+    Task AddRangeAsync(ICollection<T> entites); // bir cox entitler gelir add elemk ucun o zaman AddRangeAsync etmelisem
+    void Remove(T entity); // bir entity gelir servicesden son onu Remove edirsen bu qeder Sade 
+    void RemoveRange(ICollection<T> entites); // bir cox entitler gelir Remove elemk ucun o zaman RemoveRange etmelisem
+    void Update(T entity); // bir entity gelir ve update edirsen
+    Task SaveChangeAsync(); // SaveChangeAsync edirsen axi sonda o 
+}
