279b75430918673b5ed165121a505f3133922e6d
Kerimov056 <ulvisk@code.edu.az>
2023-07-20 15:19:49 -0700

    ICatagoryService Start

--- src/Infrastructure/UnionArchitecture.Persistence/Implementations/Services/CatagoryService.cs
+++ src/Infrastructure/UnionArchitecture.Persistence/Implementations/Services/CatagoryService.cs
@@ -1,16 +1,32 @@
 ﻿using AutoMapper;
 using UnionArchitecture.Aplication.Abstraction.Repository;
+using UnionArchitecture.Aplication.Abstraction.Repository.IEntityRepository;
 using UnionArchitecture.Aplication.Abstraction.Services;
 using UnionArchitecture.Aplication.DTOs.Catagory;
 using UnionArchitecture.Domain.Entities;
+using UnionArchitecture.Persistence.Exceptions;
 
 namespace UnionArchitecture.Persistence.Implementations.Services;
 
 public class CatagoryService : ICatagoryService
 {
-    public Task CreateAsync(CatagoryCreateDTO catagoryCreateDTO)
+    private readonly ICatagoryReadRepository _CatagoryReadRepository;
+    private readonly ICatagoryWriteRepository _CatagoryWriteRepository;
+    private readonly IMapper _mapper;
+
+    public CatagoryService(ICatagoryReadRepository catagoryReadRepository, ICatagoryWriteRepository catagoryWriteRepository, IMapper mapper)
     {
-        throw new NotImplementedException();
+        _CatagoryReadRepository = catagoryReadRepository;
+        _CatagoryWriteRepository = catagoryWriteRepository;
+        _mapper = mapper;
+    }
+
+    public async Task CreateAsync(CatagoryCreateDTO catagoryCreateDTO)
+    {
+        Catagory? catagory = await _CatagoryReadRepository
+             .GetByIdAsyncExpression(c => c.Name.ToLower().Equals(catagoryCreateDTO.name));
+        if (catagory is not null) throw new DublicatedException("Dubilcated Catagory Name!");
+
     }
 
     public Task<List<CatagoryGetDTO>> GetAllAsync()
