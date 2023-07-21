4ef7db8a15187426667a14a9a1414c49dda6bb21
Kerimov056 <ulvisk@code.edu.az>
2023-07-20 18:31:53 -0700

    IFlowersRepository && Conficirations

--- src/Infrastructure/UnionArchitecture.Persistence/Conficurations/FlowersConficurations.cs
+++ src/Infrastructure/UnionArchitecture.Persistence/Conficurations/FlowersConficurations.cs
@@ -1,15 +1,15 @@
-﻿//using Microsoft.EntityFrameworkCore;
-//using Microsoft.EntityFrameworkCore.Metadata.Builders;
-//using UnionArchitecture.Domain.Entities;
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using UnionArchitecture.Domain.Entities;
 
-//namespace UnionArchitecture.Persistence.Conficurations;
+namespace UnionArchitecture.Persistence.Conficurations;
 
-//public class FlowersConficurations : IEntityTypeConfiguration<Flowers>
-//{
-//    public void Configure(EntityTypeBuilder<Flowers> builder)
-//    {
-//        builder.Property(x => x.Name).IsRequired().HasMaxLength(50);
-//        builder.Property(x => x.ImagePath).IsRequired().HasMaxLength(500);
-//        builder.Property(x => x.Price).IsRequired();
-//    }
-//}
+public class FlowersConficurations : IEntityTypeConfiguration<Flowers>
+{
+    public void Configure(EntityTypeBuilder<Flowers> builder)
+    {
+        builder.Property(x => x.Name).IsRequired().HasMaxLength(50);
+        builder.Property(x => x.ImagePath).IsRequired().HasMaxLength(500);
+        builder.Property(x => x.Price).IsRequired();
+    }
+}
--- src/Infrastructure/UnionArchitecture.Persistence/Conficurations/FlowersDetailsConficurations.cs
+++ src/Infrastructure/UnionArchitecture.Persistence/Conficurations/FlowersDetailsConficurations.cs
@@ -1,17 +1,17 @@
-﻿//using Microsoft.EntityFrameworkCore;
-//using Microsoft.EntityFrameworkCore.Metadata.Builders;
-//using UnionArchitecture.Domain.Entities;
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using UnionArchitecture.Domain.Entities;
 
-//namespace UnionArchitecture.Persistence.Conficurations;
+namespace UnionArchitecture.Persistence.Conficurations;
 
-//internal class FlowersDetailsConficurations : IEntityTypeConfiguration<FlowersDetails>
-//{
-//    public void Configure(EntityTypeBuilder<FlowersDetails> builder)
-//    {
-//        builder.Property(x => x.Description).IsRequired().HasMaxLength(280);
-//        builder.Property(x => x.SKU).IsRequired();
-//        builder.Property(x => x.Tags).IsRequired().HasMaxLength(60);
-//        builder.Property(x => x.Weight).IsRequired();
-//        builder.Property(x => x.PowerFlowers).IsRequired().HasMaxLength(300);
-//    }
-//}
+internal class FlowersDetailsConficurations : IEntityTypeConfiguration<FlowersDetails>
+{
+    public void Configure(EntityTypeBuilder<FlowersDetails> builder)
+    {
+        builder.Property(x => x.Description).IsRequired().HasMaxLength(280);
+        builder.Property(x => x.SKU).IsRequired();
+        builder.Property(x => x.Tags).IsRequired().HasMaxLength(60);
+        builder.Property(x => x.Weight).IsRequired();
+        builder.Property(x => x.PowerFlowers).IsRequired().HasMaxLength(300);
+    }
+}
