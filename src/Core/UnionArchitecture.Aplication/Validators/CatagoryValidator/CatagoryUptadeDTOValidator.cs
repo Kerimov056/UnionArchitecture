81704849f6def312bac47318062969f2b21c3c6b
Kerimov056 <ulvisk@code.edu.az>
2023-07-22 14:02:04 -0700

    SliderValidators

---
+++ src/Core/UnionArchitecture.Aplication/Validators/CatagoryValidator/CatagoryUptadeDTOValidator.cs
@@ -0,0 +1,13 @@
+﻿using FluentValidation;
+using UnionArchitecture.Aplication.DTOs.Catagory;
+
+namespace UnionArchitecture.Aplication.Validators.CatagoryValidator;
+
+public class CatagoryUptadeDTOValidator: AbstractValidator<CatagoryCreateDTO>
+{
+	public CatagoryUptadeDTOValidator()
+	{
+        RuleFor(x => x.name).NotNull().NotEmpty().MaximumLength(34);
+        RuleFor(x => x.description).NotNull().NotEmpty().MaximumLength(500);
+    }
+}
---
+++ src/Core/UnionArchitecture.Aplication/Validators/SliderValidator/SliderCreateDTOValidator.cs
@@ -0,0 +1,14 @@
+﻿using FluentValidation;
+using UnionArchitecture.Aplication.DTOs.Slider;
+
+namespace UnionArchitecture.Aplication.Validators.SliderValidator;
+
+public class SliderCreateDTOValidator : AbstractValidator<SliderCreateDTO>
+{
+	public SliderCreateDTOValidator()
+	{
+        RuleFor(x => x.imagePath).NotNull().NotEmpty().MaximumLength(500);
+        RuleFor(x => x.title).NotNull().NotEmpty().MaximumLength(50);
+        RuleFor(x => x.description).NotNull().NotEmpty().MaximumLength(111);
+    }
+}
