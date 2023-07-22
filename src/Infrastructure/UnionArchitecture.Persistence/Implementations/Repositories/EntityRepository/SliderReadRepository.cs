6be6ef0fe11b74361cef1e21332227af0564c06b
Kerimov056 <ulvisk@code.edu.az>
2023-07-22 13:56:36 -0700

    SliderRepository

---
+++ src/Infrastructure/UnionArchitecture.Persistence/Implementations/Repositories/EntityRepository/SliderReadRepository.cs
@@ -0,0 +1,12 @@
+﻿using UnionArchitecture.Aplication.Abstraction.Repository.IEntityRepository;
+using UnionArchitecture.Domain.Entities;
+using UnionArchitecture.Persistence.Contexts;
+
+namespace UnionArchitecture.Persistence.Implementations.Repositories.EntityRepository;
+
+public class SliderReadRepository : ReadRepository<Slider>, ISliderReadRepository
+{
+    public SliderReadRepository(AppDbContext context) : base(context)
+    {
+    }
+}
