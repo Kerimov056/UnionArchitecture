53d073034043bb7d0d3e28c95c5140ea9562d96c
Kerimov056 <ulvisk@code.edu.az>
2023-07-23 01:16:21 -0700

    Blogservices's

---
+++ src/Core/UnionArchitecture.Aplication/Abstraction/Services/IBlogService.cs
@@ -0,0 +1,12 @@
+﻿using UnionArchitecture.Aplication.DTOs.Blog;
+
+namespace UnionArchitecture.Aplication.Abstraction.Services;
+
+public interface IBlogService
+{
+    Task<List<BlogGetDTO>> GetAllAsync();
+    Task AddAsync(BlogCreateDTO blogCreateDTO);
+    Task<BlogGetDTO> GetByIdAsync(Guid Id);
+    Task UpdateAsync(Guid Id,BlogUpdateDTo blogUpdateDTo);
+    Task RemoveAsync(Guid Id);
+}
---
+++ src/Infrastructure/UnionArchitecture.Persistence/Implementations/Services/BlogService.cs
@@ -0,0 +1,32 @@
+﻿using UnionArchitecture.Aplication.Abstraction.Services;
+using UnionArchitecture.Aplication.DTOs.Blog;
+
+namespace UnionArchitecture.Persistence.Implementations.Services;
+
+public class BlogService : IBlogService
+{
+    public Task AddAsync(BlogCreateDTO blogCreateDTO)
+    {
+        throw new NotImplementedException();
+    }
+
+    public Task<List<BlogGetDTO>> GetAllAsync()
+    {
+        throw new NotImplementedException();
+    }
+
+    public Task<BlogGetDTO> GetByIdAsync(Guid Id)
+    {
+        throw new NotImplementedException();
+    }
+
+    public Task RemoveAsync(Guid Id)
+    {
+        throw new NotImplementedException();
+    }
+
+    public Task UpdateAsync(Guid Id, BlogUpdateDTo blogUpdateDTo)
+    {
+        throw new NotImplementedException();
+    }
+}
