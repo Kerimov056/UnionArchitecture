1ee3716e8da0b37a661021cab74c518d2934835f
Kerimov056 <ulvisk@code.edu.az>
2023-07-24 06:34:39 -0700

    Register

--- src/Core/UnionArchitecture.Aplication/Validators/AuthValidator/RegisterDTOValidator.cs
+++ src/Core/UnionArchitecture.Aplication/Validators/AuthValidator/RegisterDTOValidator.cs
@@ -8,7 +8,7 @@ public class RegisterDTOValidator:AbstractValidator<RegisterDTO>
 {
 	public RegisterDTOValidator()
 	{
-		RuleFor(u => u.Fullname).MaximumLength(150);
+		RuleFor(u => u.Fullname).MaximumLength(150); 
 		RuleFor(u => u.Username).NotEmpty().NotNull().MaximumLength(60);
 		RuleFor(u => u.Email).EmailAddress().NotEmpty().NotNull().MaximumLength(255);
 		RuleFor(u => u.password).NotEmpty().NotNull().MaximumLength(155);
--- src/Infrastructure/UnionArchitecture.Persistence/Implementations/Services/IAuthService.cs
+++ src/Infrastructure/UnionArchitecture.Persistence/Implementations/Services/IAuthService.cs
@@ -1,16 +1,35 @@
-﻿using UnionArchitecture.Aplication.Abstraction.Services;
+﻿using Microsoft.AspNetCore.Identity;
+using System.Text;
+using UnionArchitecture.Aplication.Abstraction.Services;
 using UnionArchitecture.Aplication.DTOs.Auth;
 using UnionArchitecture.Domain.Entities.Identity;
+using UnionArchitecture.Persistence.Exceptions;
 
 namespace UnionArchitecture.Persistence.Implementations.Services;
 
 public class IAuthServic : IAuthService
 {
-    public Task Register(RegisterDTO registerDTO)
+    private readonly UserManager<AppUser> _userManager;
+    public IAuthServic(UserManager<AppUser> userManager) => _userManager = userManager;
+    public async Task Register(RegisterDTO registerDTO)
     {
         AppUser user = new()
         {
-
+            FullName = registerDTO.Fullname,
+            UserName = registerDTO.Username,
+            Email = registerDTO.Email,
+            IsActive = false
         };
+
+        IdentityResult identityUser = await _userManager.CreateAsync(user, registerDTO.password);
+        if (!identityUser.Succeeded)
+        {
+            StringBuilder errorMessage = new();
+            foreach (var error in identityUser.Errors)
+            {
+                errorMessage.AppendLine(error.Description);
+            }
+            throw new RegistrationException(errorMessage.ToString());
+        }
     }
 }
